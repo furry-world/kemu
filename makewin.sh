27c45b7d07f0f6877c7abad4003950023ec05419
qRea <queerki.rea@outlook.com>
2025-05-16 14:35:18 +0200

    executable bit my behated /j

--- makewin.sh
+++ makewin.sh
@@ -1,2 +1,3 @@
 #!/bin/sh
 x86_64-w64-mingw32-g++ main.cpp memory.cpp cpu.cpp platform.cpp -std=c++23 -I winblows/include/ -L winblows/lib/ -static -static-libgcc -static-libstdc++ -lraylib -lopengl32 -lgdi32 -lwinmm
+# TODO: put into the makefile
